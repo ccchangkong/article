3db580eda1ac3b3fe6a5bf897f43304a5e57d8b4
ccchangkong <chlo0o@qq.com>
2016-09-22 22:50:02 +0800

    add

--- js/js/23.js
+++ js/js/23.js
@@ -134,9 +134,9 @@ var SubCookieUtil = {
 	},
 	unset: function(name, subName, path, domain, secure) {
 		var subcookies = this.getAll(name);
-		if (subcookies) {
+		if(subcookies) {
 			delete subcookies[subName];
-			this.setAll(name,subcookies,null,path,domain,secure);
+			this.setAll(name, subcookies, null, path, domain, secure);
 		}
 	},
 	unsetAll: function(name, path, domain, secure) {
@@ -154,50 +154,53 @@ console.log(SubCookieUtil.get('data', 'name')); //N
 console.log(SubCookieUtil.get('data', 'book')); //JS er
 
 //设置两个cookie
-SubCookieUtil.set('data','name','N');
-SubCookieUtil.set('data','book','jsjs');
+SubCookieUtil.set('data', 'name', 'N');
+SubCookieUtil.set('data', 'book', 'jsjs');
 //设置全部子cookie和失效日期
-SubCookieUtil.setAll('data',{'data': 'name','data': 'book'},new Date('Jan 1,2010'));
+SubCookieUtil.setAll('data', {
+	'data': 'name',
+	'data': 'book'
+}, new Date('Jan 1,2010'));
 //修改名字的值，并修改cookie的失效日期
-SubCookieUtil.setAll('data','name','Mc',new Date('Jan 1,2010'));
+SubCookieUtil.setAll('data', 'name', 'Mc', new Date('Jan 1,2010'));
 
 //仅删除名为name的子cookie
-SubCookieUtil.unset('data','name');
+SubCookieUtil.unset('data', 'name');
 //删除整个cookie
 SubCookieUtil.unsetAll('data');
- 
+
 //<div style="behavior:url(#default#userData)" id=dadaStore''></div>
 
 //保存
-var dataStore=document.getElementById('dataStore');
-dataStore.setAttribute('name','N');
-dataStore.setAttribute('book','JSer');
+var dataStore = document.getElementById('dataStore');
+dataStore.setAttribute('name', 'N');
+dataStore.setAttribute('book', 'JSer');
 dataStore.save('BookInfo');
 //获取
 dataStore.load('BookInfo');
-console.log(dataStore.getAttribute('name'));//N
-console.log(dataStore.getAttribute('book'));//JSer
+console.log(dataStore.getAttribute('name')); //N
+console.log(dataStore.getAttribute('book')); //JSer
 //删除
 dataStore.removeAttribute('name');
 dataStore.removeAttribute('book');
 dataStore.save('BookInfo');
 //使用方法存储数据
-sessionStorage.setItem('name','N');
+sessionStorage.setItem('name', 'N');
 //使用属性存储数据
-sessionStorage.book='JSer';
+sessionStorage.book = 'JSer';
 //IE8 only
 sessionStorage.begin();
-sessionStorage.book='JSer';
+sessionStorage.book = 'JSer';
 sessionStorage.commit();
 
 //使用方法读取数据
-var name =sessionStorage.getItem('name');
+var name = sessionStorage.getItem('name');
 //使用属性读取数据
-var book=sessionStorage.book;
+var book = sessionStorage.book;
 
-for (var i=0,len=sessionStorage.length;i<len;i++) {
-	var value=sessionStorage.getItem(key);
-	console.log(key+'='+value);
+for(var i = 0, len = sessionStorage.length; i < len; i++) {
+	var value = sessionStorage.getItem(key);
+	console.log(key + '=' + value);
 }
 
 //使用delete删除一个值
@@ -206,70 +209,121 @@ delete sessionStorage.name;
 sessionStorage.removeItem('book');
 
 //保存数据
-globalStorage['worx.com'].name='N';
+globalStorage['worx.com'].name = 'N';
 //获取数据
-var name= globalStorage['worx.com'].name;
+var name = globalStorage['worx.com'].name;
 //worx.com及其子域都可以访问
-globalStorage['www.worx.com'].name='N';
-var name= globalStorage['www.worx.com'].name;
-
+globalStorage['www.worx.com'].name = 'N';
+var name = globalStorage['www.worx.com'].name;
 
 //存储数据，任何人都可以访问-不要这么做
-globalStorage[''].name="N";
+globalStorage[''].name = "N";
 //存储数据，可以让任何以.net结尾的域名访问-不要这么做
-globalStorage['net'].name='N';
+globalStorage['net'].name = 'N';
 
-globalStorage['www.wrox.com'].name='N';
-globalStorage['www.wrox.com'].book='JSer';
-globalStorage['www.wrox.com'].removeItem('name');//记得删哦
-var book=globalStorage['www.wrox.com'].getItem('book');
+globalStorage['www.wrox.com'].name = 'N';
+globalStorage['www.wrox.com'].book = 'JSer';
+globalStorage['www.wrox.com'].removeItem('name'); //记得删哦
+var book = globalStorage['www.wrox.com'].getItem('book');
 //localhost.host做属性名，稳
-globalStorage[location.host].name='N';
-var book=globalStorage[location.host].getItem('book');
+globalStorage[location.host].name = 'N';
+var book = globalStorage[location.host].getItem('book');
 
 //使用方法存储数据
-localStorage.setItem('name','N');
+localStorage.setItem('name', 'N');
 //使用属性存储数据
-localStorage.book='JSer';
+localStorage.book = 'JSer';
 //使用方法读取数据
-var name=localStorage.getItem('name');
+var name = localStorage.getItem('name');
 //使用属性读取数据
-var book=localStorage.book;
+var book = localStorage.book;
 
-function getLocalStorage () {
-	if (typeof localStorage=='object') {
+function getLocalStorage() {
+	if(typeof localStorage == 'object') {
 		return localStorage;
-	} else if(typeof globalStorage=='object'){
+	} else if(typeof globalStorage == 'object') {
 		return globalStorage[location.host];
-	}else{
+	} else {
 		throw new Error('local storage not available');
 	}
 }
-var storage=getLocalStorage();
+var storage = getLocalStorage();
 
-EventUtil.addHandler(document,'storage',function (event) {
-	console.log('storage changed for'+event.domain);
+EventUtil.addHandler(document, 'storage', function(event) {
+	console.log('storage changed for' + event.domain);
 });
 
-var indexedDB=window.indexedDB||window.msIndexedDB||window.mozIndexedDB||window.webkitIndexedDB;
+var indexedDB = window.indexedDB || window.msIndexedDB || window.mozIndexedDB || window.webkitIndexedDB;
 
-var request=indexedDB.open('admin');
-request.onerror=function (event) {
-	console.log('something bad happend while trying to open:'+event.target.errorCode);
+var request = indexedDB.open('admin');
+request.onerror = function(event) {
+	console.log('something bad happend while trying to open:' + event.target.errorCode);
 };
-request.onsuccess=function (event) {
-	database=event.target.result;
+request.onsuccess = function(event) {
+	database = event.target.result;
 };
 
 //设置版本号
-if (database.version!='1.0') {
-	request=database.setVersion('1.0');
-	request.onerror=function (event) {
-		console.log('set version error:'+event.target.errorCode);
+if(database.version != '1.0') {
+	request = database.setVersion('1.0');
+	request.onerror = function(event) {
+		console.log('set version error:' + event.target.errorCode);
 	};
-	request.onsuccess=function (event) {
-		console.log('complete.Database name:'+database.name+',version:'+database.version);
+	request.onsuccess = function(event) {
+		console.log('complete.Database name:' + database.name + ',version:' + database.version);
 	};
-} else{
-	console.log('Database already initialized.Database name:'+database.name+',version:'+database.version)
+} else {
+	console.log('Database already initialized.Database name:' + database.name + ',version:' + database.version)
 }
+
+var user = {
+	username: '007',
+	firstName: 'James',
+	lastName: 'Bond',
+	password: 'foo'
+};
+//示例
+var store = db.createObjectStore('users', {
+	keyPath: 'username'
+});
+//users中保存着一批用户数据
+var i = 0,
+	request,
+	requests = [],
+	len = users.length;
+while(i < len) {
+	request = store.add(users[i++]);
+	request.onerror = function() {
+		//处理错误
+	};
+	request.onsuccess = function() {
+		//处理成功
+	};
+	requests.push(request);
+};
+
+//db=database
+var transaction = db.transaction();
+//传入要访问的对象存储空间
+var transaction = db.transaction('users');
+var transaction = db.transaction(['users', 'anotherStore']);
+
+//设定访问模式，读写什么的
+var IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction;
+var transaction = db.transaction('users', IDBTransaction.READ_WRITE);
+
+var request = db.transaction('users').objectStore('users').get('007');
+request.onerror = function(event) {
+	console.log('did not get the object');
+};
+request.onsuccess = function(event) {
+	var result = event.target.result;
+	console.log(result.firstName); //James
+};
+
+transaction.onerror = function(event) {
+	//整个事务都被取消了
+};
+transaction.oncomplete = function(event) {
+	//整个事务都成功完成了
+};
